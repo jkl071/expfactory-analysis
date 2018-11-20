c6d5645643f7095392d22790d695a6cbaec9f64b
Jamie Li <jamie@jamies-mbp.attlocal.net>
2018-11-20 14:47:13 -0800

    caching my changes

--- expanalysis/api.py
+++ expanalysis/api.py
@@ -2,18 +2,47 @@
 expanalysis.api: functions for retrieving experiment factory results
 
 """
-
+import pandas as pd
+import os
 from expanalysis.utils import get_pages
 
-def get_results(url=None,access_token=None, last_url=None):
+def findWorker(arg):
+    found = 0
+    for i in range(0, len(results)):
+        if results[i]['worker']['id'] == arg:
+            print("found worker, please look at index: " + str(i) )
+            found = found + 1
+    if found == 0:
+        print("No worker by that ID")
+        
+        
+        
+def get_results(url=None,access_token=None):
     '''get_results is a wrapper for get_url, to first check that the user has provided an access token
     :param url: the expfactory/results/api url
     :param access_token: a token obtained at expfactory.org/token when the user is logged in
     '''
     if url == None:
-        url = "http://www.expfactory.org/api/results"
+        url = "http://expfactory.org/new_api/results/81/"
     if access_token != None:
-        return get_pages(url=url,access_token=access_token, last_url=last_url)
+        return get_pages(url=url,access_token=access_token)
     else:
         print("You must provide an access_token to authenticate to the API.")
    
+access_token = "5163be7d0a6a347ebce46dac018547a6846ed47c" # expfactory.org/token
+results = get_results(access_token=access_token)
+   
+#This snippet creates a folder called expfactory_online_data in your /Users/elliott/Desktop directory.  
+#If you don't like the path where the folder is created, change the file path variable to your desired location
+file_path = "~/Desktop/expfactory_online_data"
+if not os.path.exists(file_path):
+    os.makedirs(file_path)
+
+#Line 69 returns each sub as a csv file, inside the directory, /Desktop/expfactory_online_data
+#change the directory given in order to match up with yours.
+all_subs_df = []
+for i in range(0, len(results)):
+    if results[i]['completed'] == True:
+        single_sub_df = pd.DataFrame(results[i]['data'][0]['trialdata'])
+        single_sub_df.to_csv('~/Desktop/expfactory_online_data/all_subs_df_'+str(i)+'.csv',sep=',')
+        
--- expanalysis/utils.py
+++ expanalysis/utils.py
@@ -1,15 +1,14 @@
 """
-analysis/utils.py: part of expfactory package
-functions for working with experiment factory results
-"""
 
+functions for working with experiment factory results
+
+"""
 import requests
 import __init__
 import pandas
 import json
 import os
-import unicodedata
-import re
+
 
 def get_installdir():
     '''get_installdir returns the install directory of the package'''
@@ -27,7 +26,7 @@ def save_json(json_obj,output_file):
     return output_file
 
 
-def get_pages(url=None,access_token=None,last_url=None):
+def get_pages(url=None,access_token=None):
     '''get_url retrieves the data at the experiment factory results page. The user must provide authentication, and the function assumes paginated results.
     :param url: the url to retrieve, default is expfactory.org/api/results
     :param access_token: access token retrieved at expfactory.org/token
@@ -36,29 +35,26 @@ def get_pages(url=None,access_token=None,last_url=None):
         url = "http://www.expfactory.org/api/results"
 
     if access_token != None:
-        headers = {"Authorization":"token %s" %(access_token)}    
-
+        headers = {"Authorization":"token %s" %(access_token)}
+        
     results = []
 
     # Continue retrieving pages until there is no next page
     while url != None:
-       print("Retrieving %s" %(url))
+       print "Retrieving %s" %(url)
        r = get_url(url,headers=headers)
        if r.status_code == 200:
            data = r.json()
            results = results + data["results"]
            url = data["next"]
-           if last_url != None and url == last_url:
-              break
        else:       
-           print("Error: %s" %(r.reason))
-           if (r.reason) == 'UNAUTHORIZED':
-               break
-    print("Found %s results!" %(len(results)))
+           print "Error: %s" %(r.reason)
+    print "Found %s results!" %(len(results))
     
     return results
      
     
+
 def get_url(url,headers=None):
     '''get_url returns a url, with params embedded in the header
     :param url: the url to retrieve
